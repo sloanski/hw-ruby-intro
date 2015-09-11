9908e9fda83e61f0b097fd69913e1ef6c00a833e
sloanski <wsloane21@gmail.com>
2015-09-11 01:58:00 +0000

    Finished parts 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,85 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  total = 0
+  arr.each do |x|
+    puts x
+    total += x
+end
+total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  arr.sort!
+  arr[-1] + arr[-2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
+
+  #(arr.empty? && arr.length == 1) || arr.permutation(2).any? { |a, b| a + b == n }
+  
+  if arr.empty? || arr.length == 1
+    return false
+  else if arr.permutation(2).any? { |a, b| a + b == n }
+    return true
+  else
+    return false
+  end
+  end
+end 
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  #letters = s.split("")
+  #return false if s.empty?
+  #return true if s =~ /[0-9]/
+ # return true if letters[0] == /[aeiouAEIOU*]/
+  #return true if s.starts_with(/[aeiouAEIOU*]/)
+   #s =~ /^[^aeiou]/i 
+   #return false if s.empty?
+   #return true if s =~ /\A[^aeiou]/
+   return true if s[0] =~ /[bcdfghjklmnprstvwxyz]+/i
+   
+ 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #return false if s =~ /[A-Za-z]/
+  if s =~ /^[0-1]+$/
+  return s.to_i(2) % 4 == 0
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end 
+  
+ 
+ 
+  
+  def price_as_string? r
+    return number_to_currency(r, precision: 2)
+  end
+    
+  
+  
+  
 end
